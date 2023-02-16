9ea60523615e4fe73fe8c4a817bdd7b378e22f6f
EgonRasmussen <ecr@eucsyd.dk>
2023-02-16 09:21:15 +0100

    Virker

---
+++ src/configure.h
@@ -0,0 +1,22 @@
+// Virker med WiFiNINA firmware v. 1.5.0
+
+// Azure IoT Central device information
+static const char PROGMEM iotc_scopeId[] = "0ne0092420A";
+static const char PROGMEM iotc_deviceId[] = "MKRWiFi1010";
+static const char PROGMEM iotc_deviceKey[] = "UqbS6wPmb5k8QOkz1vn6qHUylwQf7aov+dM4xGJd+Dg=";
+static const char PROGMEM iotc_hubHost[] = "iotc-4f14026e-7a3c-4640-8918-7eb166d81b10.azure-devices.net";
+
+// Wi-Fi information
+static char PROGMEM wifi_ssid[] = "ORBI30";
+static char PROGMEM wifi_password[] = "15711571";
+
+// comment / un-comment the correct sensor type being used
+#define SIMULATE_DHT_TYPE
+//#define DHT11_TYPE
+//#define DHT22_TYPE
+
+// for DHT11/22, 
+//   VCC: 5V or 3V
+//   GND: GND
+//   DATA: 2
+int pinDHT = 2;
